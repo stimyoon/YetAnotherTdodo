24e30da8cb4b2f65abf9102286064a3613a6b5f7
stimyoon <64033604+stimyoon@users.noreply.github.com>
2022-12-22 20:23:02 -0500

    Initial Commit

---
+++ YetAnotherTdodo/YetAnotherTdodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YetAnotherTdodoApp.swift
+//  YetAnotherTdodo
+//
+//  Created by Tim Yoon on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct YetAnotherTdodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
